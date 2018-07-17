1c0a6a70cb3a77412648790cda6cb6b3bc3b3b6b
ndroidapp <ndroidapp.in@gmail.com>
2018-07-17 16:57:40 +0530

    animation added

--- app/src/main/java/com/turningideas/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/turningideas/qrcodescanner/MainActivity.java
@@ -1,55 +1,72 @@
 package com.turningideas.qrcodescanner;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtView;
+    TextView tvInfo;
+    ImageView ivScan;
 
-    public static final int REQUEST_CODE=1001;
-    public static final int PERMISSION_REQUEST=2001;
+    public static final int REQUEST_CODE = 1001;
+    public static final int PERMISSION_REQUEST = 2001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn =  findViewById(R.id.button);
-        txtView = findViewById(R.id.txtContent);
+        tvInfo = findViewById(R.id.tvInfo);
+        ivScan = findViewById(R.id.ivScan);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},PERMISSION_REQUEST);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
         }
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        ivScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,ScanActivity.class);
-                startActivityForResult(intent,REQUEST_CODE);
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode==REQUEST_CODE && resultCode==RESULT_OK){
-            if (data!=null){
-                final Barcode barcode=data.getParcelableExtra("barcode");
-                txtView.post(new Runnable() {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                final Barcode barcode = data.getParcelableExtra("barcode");
+                tvInfo.post(new Runnable() {
                     @Override
                     public void run() {
-                        txtView.setText(barcode.displayValue);
+                        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+                        } else {
+                            vibrator.vibrate(50);
+                        }
+                        ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                        toneGenerator.startTone(ToneGenerator.TONE_CDMA_PIP, 50);
+
+                        tvInfo.setText(barcode.displayValue);
                     }
                 });
             }
--- app/src/main/java/com/turningideas/qrcodescanner/ScanActivity.java
+++ app/src/main/java/com/turningideas/qrcodescanner/ScanActivity.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
@@ -30,7 +34,13 @@ public class ScanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.fade);
+
+
         cameraView = findViewById(R.id.cameraView);
+
+        cameraView.setAnimation(animation);
+
         cameraView.setZOrderMediaOverlay(true);
         surfaceHolder = cameraView.getHolder();
         barcodeDetector = new BarcodeDetector.Builder(this)
@@ -83,10 +93,17 @@ public class ScanActivity extends AppCompatActivity {
                     intent.putExtra("barcode", barcodeSparseArray.valueAt(0));
                     setResult(RESULT_OK, intent);
                     finish();
+                    ScanActivity.this.overridePendingTransition(0, 0);
                 }
 
             }
         });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        barcodeDetector.release();
     }
 }
